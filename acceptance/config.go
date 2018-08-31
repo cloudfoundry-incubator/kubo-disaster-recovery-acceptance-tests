0ba6937c1cae6f10216e66120cad05b05f40ecf4
Josh Hill <jhill@pivotal.io>
2018-08-31 09:56:07 +0100

    Inline kubo config in config.json
    [#160089219]

--- acceptance/config.go
+++ acceptance/config.go
@@ -4,13 +4,9 @@ import "time"
 
 type Config struct {
 	TimeoutMinutes time.Duration `json:"timeout_in_minutes"`
-	Kubo           KuboConfig    `json:"kubo"`
-}
-
-type KuboConfig struct {
-	ClusterName  string `json:"cluster_name"`
-	APIServerURL string `json:"api_server_url"`
-	CACert       string `json:"ca_cert"`
-	Username     string `json:"username"`
-	Password     string `json:"password"`
+	ClusterName    string        `json:"cluster_name"`
+	APIServerURL   string        `json:"api_server_url"`
+	CACert         string        `json:"ca_cert"`
+	Username       string        `json:"username"`
+	Password       string        `json:"password"`
 }
