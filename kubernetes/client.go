af9490b02cea69eb540562eacf4d25a0159ab9f4
Sarah Connor <sconnor@pivotal.io>
2019-03-12 16:44:01 +0000

    Add oidc to pull in the package
    [#163629171]
    
    Signed-off-by: Emmanouil Kiagias <ekiagias@pivotal.io>

--- kubernetes/client.go
+++ kubernetes/client.go
@@ -12,6 +12,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 	restclient "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
